2f148fc35b88a3f7887f5bc9c6cbc30bfdf50ea1
Keith Saniga <sanigak@outlook.com>
2019-05-10 10:18:36 -0400

    voice to text is operational

--- Babelfish-Stack/Babelfish_Stack.py
+++ Babelfish-Stack/Babelfish_Stack.py
@@ -1,1 +1,21 @@
+import speech_recognition as sr
 
+
+r = sr.Recognizer()
+mic = sr.Microphone()
+
+#harvard = sr.AudioFile('harvard.wav')
+#with harvard as source:
+#    audio = r.record(source)
+
+with mic as source:
+    r.adjust_for_ambient_noise(source)
+    audio = r.listen(source)
+
+
+
+
+try:
+    print(r.recognize_google(audio))
+except:
+    print("Audio cannot be recognized")
